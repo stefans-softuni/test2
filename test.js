117460bac70c3568f929a2bd31d8cbd7ccfe6494
stefans-softuni <stefans.softuni@gmail.com>
2024-09-16 23:08:37 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+    console.log(a + b);
+}
+
+add(5, 3);
+
+function saberi(a, b) {
+    console.log(a + b);
+}
+
+saberi(6, 8);
+
+function oduzmi(a, b) {
+    console.log(a - b);
+}
+
+oduzmi(5, 3);
---
+++ test2.js
@@ -0,0 +1,36 @@
+function solve(type, age) {
+    if (type == 'Weekday') {
+        if (age > 64 && age <= 122) {
+            console.log('12$');
+        } else if (age > 18 && age <= 64) {
+            console.log('18$');
+        } else if (age >= 0 && age <= 18) {
+            console.log('12$');
+        } else {
+            console.log('Error!');
+        }
+    } else if (type == 'Weekend') {
+        if (age > 64 && age <= 122) {
+            console.log('15$');
+        } else if (age > 18 && age <= 64) {
+            console.log('20$');
+        } else if (age >= 0 && age <= 18) {
+            console.log('15$');
+        } else {
+            console.log('Error!');
+        }
+    } else if (type == 'Holiday') {
+        if (age > 64 && age <= 122) {
+            console.log('10$');
+        } else if (age > 18 && age <= 64) {
+            console.log('12$');
+        } else if (age >= 0 && age <= 18) {
+            console.log('5$');
+        } else {
+            console.log('Error!');
+        }
+    }
+
+}
+
+solve("Holiday", 5);
